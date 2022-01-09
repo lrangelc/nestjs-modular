ebc6a52865e817995c89d4cff49a576b52bc08f3
Luis Rangel <luisrangelc@gmail.com>
2022-01-09 13:51:13 -0600

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsService } from './services/products.service';
+
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService],
+  imports: [UsersModule, ProductsModule, CategoriesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
+
+@Module({
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
 export class ProductsModule {}
