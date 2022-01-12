a1ccf8074f67b3df35d2186856561c1612ce9e68
Luis Rangel <luisrangelc@gmail.com>
2022-01-11 23:21:40 -0600

    add tags

--- src/products/dtos/products.dto.ts
+++ src/products/dtos/products.dto.ts
@@ -5,15 +5,18 @@ import {
   IsNotEmpty,
   IsPositive,
 } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Product name' })
   readonly name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Product description' })
   readonly description: string;
 
   @IsNumber()
@@ -31,4 +34,4 @@ export class CreateProductDto {
   readonly image: string;
 }
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) { }
